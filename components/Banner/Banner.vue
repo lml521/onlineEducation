0e3f1b8963c6ce033998a428e43a70d35c191f25
lml <2084387897@qq.com>
2022-12-04 20:34:29 +0800

    封装轮播图

---
+++ components/Banner/Banner.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="banner">
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+			<swiper-item class="flex justify-center" v-for="(item,index) in list " :key="index">
+				<img :src="item.img" alt="">
+			</swiper-item>
+			
+		</swiper>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"Banner",
+		props:{
+			list:{
+				type:Array,
+				default:()=>[]
+			}
+		},
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.banner,swiper{
+	height: 300rpx;
+	swiper-item{
+		img{
+			width: 720rpx;
+			height: 100%;
+		}	
+	}
+	
+	
+}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,22 +1,40 @@
 <template>
-	<view class="content animate__animated animate__fadeInDown">
-			124
-		<view class="text-area">
+	<!-- <view class="content animate__animated animate__fadeInDown"> -->
+			<!-- 124 -->
+		<!-- <view class="text-area">
 			<text class="title text-main">{{title}}</text>
 			<view class="animated bounceOutLeft"></view>
 			<view class="iconfont icon-info">
 				
-			</view>
+			</view> -->
+		<!-- </view> -->
+	<!-- </view> -->
+	<view class="box">
+		<view class="banner">
+			<Banner :list="BannerList"></Banner>
 		</view>
+			
+	
+		
+		
 	</view>
 </template>
 
 <script>
-	import api from '@/api/login.js'
+import Banner from '@/components/Banner/Banner.vue'
 	export default {
+		components:{
+			Banner
+		},
 		data() {
 			return {
-				title: 'Hello'
+				BannerList:[{
+					id:1,
+					img:"/static/banner/banner1.png",
+				},{
+					id:2,
+					img:"/static/banner/banner2.png",
+				}]
 			}
 		},
 		onLoad() {
@@ -26,16 +44,14 @@
 			this.login()
 		},
 		methods: {
-			async login(){
-				let res =await api.login()
-				// console.log(res)
-				
-			}
+		
 
 		}
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+.banner{
+	// padding: 0 10rpx;
+}
 </style>
