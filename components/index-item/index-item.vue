1136dc754289528e814510fe29b45a14d1eb560d
lml <2084387897@qq.com>
2022-12-06 08:24:32 +0800

    封装首页子组件item

---
+++ components/index-item/index-item.vue
@@ -0,0 +1,119 @@
+<template>
+	<!-- 每一个盒子 -->
+	<!-- @click="navTo(`/pages/course/course-details?id=${item.id}`)" -->
+	<view class="item-box" :class="{column : isColumn}">
+		<view class="left">
+			<img :src="item.cover" alt="">
+			<text class="item-time">{{item.title=='media'?'视频':'图文'}}</text>
+		</view>
+		<view class="right flex flex-column">
+			<view class="right-title text-ellipsis  font-md">{{item.title}}</view>
+<view class="flex flex-1 align-end count">
+	<view  v-if="item.price">￥{{item.price}}</view>
+	<view class="grey" >￥{{item.t_price}}</view>
+</view>
+			<!-- <view class="info">
+				<view class="nickname">
+					<i class="iconfont icon-laoshi2"></i>
+					{{item.nickName}}
+				</view> -->
+			<!-- 	<view class="count">
+					<text v-if="item.isFree" class="money">免费</text>
+					<text v-else class="money iconfont icon-moneybag">{{item.priceDiscount||item.priceOriginal}}</text>
+					<text class="iconfont icon-video">{{item.studyTotal}} 人在学</text>
+				</view> -->
+			<!-- </view> -->
+
+
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "course-item",
+		props: {
+
+			isColumn: {
+				type: Boolean,
+				default: false
+			},
+
+			item: {
+				type: Object,
+				default: () => {
+					return {
+						cover: "http://demo-mp3.oss-cn-shenzhen.aliyuncs.com/egg-edu-demo/10ccf3a973f5193bec3c.png",
+						end_time: "2024-06-29T16:00:00.000Z",
+						group_id: 29,
+						id: 6,
+						price: "5.00",
+						start_time: "2021-05-15T17:04:00.000Z",
+						t_price: "9.98",
+						title: "VueCli 实战在线教育后台系统",
+						type: "media",
+					}
+
+				}
+			}
+		},
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.item-box {
+		display: flex;
+		width: 100%;
+		padding: 20rpx 10rpx;
+		border-bottom: 2rpx solid #ccc;
+
+		.left {
+			width: 300rpx;
+			height: 170rpx;
+			position: relative;
+			margin-right: 20rpx;
+
+			img {
+				width: 100%;
+				height: 100%;
+			}
+
+			.item-time {
+				position: absolute;
+				bottom: 0;
+				background-color: rgba(0, 0, 0, .2);
+				padding: 3rpx 8rpx;
+				font-size: 14rpx;
+				right: 0;
+				color: #fff;
+			}
+		}
+
+		.right {
+		margin-right: 22rpx;
+		flex: 1;
+			.right-title {
+				overflow: hidden;
+				text-overflow: ellipsis;
+				-webkit-line-clamp: 1;
+				display: -webkit-box;
+				-webkit-box-orient: vertical;
+				white-space: normal;
+			}
+			.count{
+				view{
+					font-size: 38rpx;
+					color: #dc3545;
+				}
+				.grey{
+					font-size: 28rpx;
+									color: grey;}
+			}
+		}
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -9,6 +9,19 @@
 		<!-- discountCoupon -->
 		<coupon :couponList="couponList"></coupon>
 
+		<view class="divider"></view>
+		<h4 class="flex align-center py-3 px-2">
+		<text class="font-md font-weight-bold">
+			拼团	
+		</text>
+		</h4>
+		<indexItem></indexItem>
+		
+		<view class="divider"></view>
+		<h4 class="flex align-center py-3 px-2 justify-between">
+		<text class="font-md font-weight-bold">最新列表</text>
+		<text class="font-sm text-light-muted">查看更多</text>
+		</h4>
 	</view>
 </template>
 
@@ -17,11 +30,13 @@
 	import indexApi from "@/api/index.js" //api请求
 	import multiRowNav from '@/components/multi-row-nav/multi-row-nav.vue' //nav数据组件
 	import coupon from '@/components/coupon/coupon.vue'
+	import indexItem from "@/components/index-item/index-item.vue"
 	export default {
 		components: {
 			Banner,
 			multiRowNav,
-			coupon
+			coupon,
+			indexItem
 		},
 		data() {
 			return {
